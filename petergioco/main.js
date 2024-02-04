7260cd7be9fff9e22babf1726484bad373a3d19b
CaliPanni <97522907+CaliPanni@users.noreply.github.com>
2024-02-04 12:28:24 +0100

    Update main.js

--- petergioco/main.js
+++ petergioco/main.js
@@ -1,12 +1,24 @@
 var swfobject = {};
 
-swfobject.embedSWF = function(url, cont, width, height){
+swfobject.embedSWF = function(url, cont){
     var ruffle = window.RufflePlayer.newest(),
-        player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()), {
-            width: width,
-            height: height,
-            style: 'width: ' + width + 'px; height: ' + height + 'px',
+        container = document.getElementById(cont),
+        player = Object.assign(container.appendChild(ruffle.createPlayer()), {
+            style: 'width: 100%; height: 100%;',
         });
 
     player.load({ url: url });
+
+    // Aggiungi il supporto per il fullscreen
+    container.addEventListener('click', function() {
+        if (container.requestFullscreen) {
+            container.requestFullscreen();
+        } else if (container.mozRequestFullScreen) {
+            container.mozRequestFullScreen();
+        } else if (container.webkitRequestFullscreen) {
+            container.webkitRequestFullscreen();
+        } else if (container.msRequestFullscreen) {
+            container.msRequestFullscreen();
+        }
+    });
 }
