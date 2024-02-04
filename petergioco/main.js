fa11ee17f488f51412d8bacb7d3c310e005fd934
CaliPanni <97522907+CaliPanni@users.noreply.github.com>
2024-02-04 13:59:23 +0100

    Update main.js

--- petergioco/main.js
+++ petergioco/main.js
@@ -3,7 +3,9 @@ var swfobject = {};
 swfobject.embedSWF = function(url, cont, width, height){
     var ruffle = window.RufflePlayer.newest(),
         player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()), {
-            style="min-height:100vh;width:100%"
+            width: width,
+            height: height,
+            style: 'width: ' + width + 'px; height: ' + height + 'px',
         });
 
     player.load({ url: url });
